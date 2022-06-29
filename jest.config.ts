b0125d39dfe3c7ed1f41799603006bef74c6c231
Progyan Bhattacharya <bprogyan@gmail.com>
2022-06-29 18:59:05 +0530

    (chore) AuthorController: Add Unit Tests
    Signed-off-by: Progyan Bhattacharya <bprogyan@gmail.com>

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+/**
+ * Configuration for Jest
+ */
+import { pathsToModuleNameMapper } from "ts-jest";
+import { Config } from "@jest/types";
+import tsconfig = require("./tsconfig.json");
+
+const Configuration: Config.InitialOptions = {
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.(t|j)s"],
+  coveragePathIgnorePatterns: ["src/configs", "src/main"],
+  coverageDirectory: "coverage",
+  moduleFileExtensions: ["js", "json", "ts"],
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
+    prefix: "<rootDir>/",
+  }),
+  rootDir: ".",
+  testEnvironment: "node",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest",
+  },
+};
+
+export default Configuration;
--- src/controllers/author.controller.spec.ts
+++ src/controllers/author.controller.spec.ts
@@ -1,22 +1,212 @@
-import { Test, TestingModule } from "@nestjs/testing";
+import { CreateAuthorDto, UpdateAuthorDto } from "@dtos/author.dto";
+import { CreateBookDto } from "@dtos/book.dto";
+import { Author } from "@entities/author.entity";
+import { Book } from "@entities/book.entity";
 import { AuthorService } from "@services/author.service";
+import { BookService } from "@services/book.service";
+import { plainToInstance } from "class-transformer";
+import { Repository } from "typeorm";
 
 import { AuthorController } from "./author.controller";
 
 describe("AuthorController", () => {
-  let app: TestingModule;
+  let authorController: AuthorController;
+  let authorService: AuthorService;
+  let bookService: BookService;
 
-  beforeAll(async () => {
-    app = await Test.createTestingModule({
-      controllers: [AuthorController],
-      providers: [AuthorService],
-    }).compile();
+  beforeEach(() => {
+    authorService = new AuthorService(null as Repository<Author>);
+    bookService = new BookService(null as Repository<Book>);
+    authorController = new AuthorController(authorService, bookService);
   });
 
-  describe("geAuthors", () => {
-    it('should return "Hello World!"', () => {
-      const appController = app.get<AuthorController>(AuthorController);
-      expect(appController.getAuthors()).toBe("Hello World!");
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("getAuthors", () => {
+    it("should return an array of authors", async () => {
+      const authors = [
+        plainToInstance(Author, {
+          id: 1,
+          firstName: "Makashi",
+          lastName: "Kishimoto",
+        }),
+      ];
+      jest.spyOn(authorService, "findAll").mockResolvedValueOnce(authors);
+
+      expect(await authorController.getAuthors()).toEqual(authors);
+    });
+  });
+
+  describe("getAuthor", () => {
+    it("should return an author with id", async () => {
+      const author = plainToInstance(Author, {
+        id: 1,
+        firstName: "Makashi",
+        lastName: "Kishimoto",
+      });
+      jest.spyOn(authorService, "findOne").mockResolvedValueOnce(author);
+
+      expect(await authorController.getAuthor(author.id)).toEqual(author);
+    });
+  });
+
+  describe("getBooks", () => {
+    it("should return array of books by the author with id", async () => {
+      const books = [
+        plainToInstance(Book, {
+          id: 1,
+          name: "Naruto",
+        }),
+        plainToInstance(Book, {
+          id: 1,
+          name: "Naruto Shipuden",
+        }),
+      ];
+      const author = plainToInstance(Author, {
+        id: 1,
+        firstName: "Makashi",
+        lastName: "Kishimoto",
+        books,
+      });
+      jest.spyOn(authorService, "findOne").mockResolvedValueOnce(author);
+
+      expect(await authorController.getBooks(author.id)).toEqual(author.books);
+    });
+  });
+
+  describe("addAuthor", () => {
+    it("should return an author with given name", async () => {
+      const authorDto = plainToInstance(CreateAuthorDto, {
+        firstName: "Makashi",
+        lastName: "Kishimoto",
+      });
+      const author = plainToInstance(Author, {
+        id: 1,
+        firstName: "Makashi",
+        lastName: "Kishimoto",
+      });
+      jest.spyOn(authorService, "save").mockImplementationOnce(async (dto) => ({
+        id: author.id,
+        ...dto,
+      }));
+
+      expect(await authorController.addAuthor(authorDto)).toEqual(author);
+    });
+  });
+
+  describe("addBook", () => {
+    it("should return an author with given book", async () => {
+      const bookDto = plainToInstance(CreateBookDto, {
+        name: "Naruto",
+      });
+      const author = plainToInstance(Author, {
+        id: 1,
+        // firstName: "Makashi",
+        // lastName: "Kishimoto",
+      });
+      const book = plainToInstance(Book, {
+        id: 1,
+        name: "Naruto",
+        authors: [author],
+      });
+
+      jest.spyOn(bookService, "save").mockImplementationOnce(async (dto) => ({
+        id: book.id,
+        ...dto,
+      }));
+
+      expect(await authorController.addBook(author.id, bookDto)).toEqual(book);
+    });
+  });
+
+  describe("addOrUpdateAuthor", () => {
+    it("should return an author with given name and id", async () => {
+      const authorDto = plainToInstance(UpdateAuthorDto, {
+        id: 1,
+        firstName: "Hajime",
+        lastName: "Isamaya",
+      });
+      const author = plainToInstance(Author, {
+        id: 1,
+        firstName: "Makashi",
+        lastName: "Kishimoto",
+      });
+      jest.spyOn(authorService, "save").mockImplementationOnce(async (dto) => ({
+        ...author,
+        ...dto,
+      }));
+
+      expect(await authorController.addOrUpdateAuthor(authorDto)).toEqual({
+        ...author,
+        ...authorDto,
+      });
+    });
+  });
+
+  describe("updateAuthor", () => {
+    it("should return an author with given id and name", async () => {
+      const authorDto = plainToInstance(UpdateAuthorDto, {
+        firstName: "Hajime",
+        lastName: "Isayama",
+      });
+      const author = plainToInstance(Author, {
+        id: 1,
+        firstName: "Hajime",
+        lastName: "Isayama",
+      });
+      jest
+        .spyOn(authorService, "update")
+        .mockImplementationOnce(async (id, dto) => ({
+          id,
+          ...dto,
+        }));
+
+      expect(await authorController.updateAuthor(author.id, authorDto)).toEqual(
+        author,
+      );
+    });
+  });
+
+  describe("deleteAuthor", () => {
+    it("should delete an author with given id", async () => {
+      const author = plainToInstance(Author, {
+        id: 1,
+        firstName: "Makashi",
+        lastName: "Kishimoto",
+      });
+      const deleteSpy = jest
+        .spyOn(authorService, "delete")
+        .mockReturnValueOnce(undefined);
+
+      await authorController.deleteAuthor(author.id);
+      expect(deleteSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe("removeAuthor", () => {
+    it("should delete a book from an author with given id", async () => {
+      const book = plainToInstance(Book, {
+        id: 1,
+        name: "Naruto",
+      });
+      const author = plainToInstance(Author, {
+        id: 1,
+        firstName: "Makashi",
+        lastName: "Kishimoto",
+        books: [book],
+      });
+      jest.spyOn(authorService, "findOne").mockResolvedValueOnce(author);
+      const saveSpy = jest
+        .spyOn(authorService, "save")
+        .mockImplementationOnce(async (dto) => dto);
+
+      await authorController.removeBook(author.id, book.id);
+      expect(saveSpy).toHaveBeenCalledWith({
+        ...author,
+        books: Promise.resolve(),
+      });
     });
   });
 });
